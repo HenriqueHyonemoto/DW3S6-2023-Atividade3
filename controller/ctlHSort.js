2af4379f2f689f6707bb98254dc78f14dd7001b8
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2023-11-27 19:59:18 -0300

    Condição de 2 a 20 números

--- controller/ctlHSort.js
+++ controller/ctlHSort.js
@@ -5,23 +5,37 @@ const hello = (req, res) => (async () => {
 
 //função para ordenar o vetor e mostra-lo ordenado
 const ordenaVetor = (req, res) => (async () => {
+    
+    //Pegando os valores mandado pelo POST do tests.rest
     const { vetor } = req.body;
-    vetor.sort((b, a) => b - a);
-    res.json({ status: 'ok', Números_Ordenados: vetor });
+
+    //Verifica se o array possui menos que 2 valores ou mais que 20, nesse caso ele informa um erro
+    if (vetor.length < 2 || vetor.length >20 ) {
+        res.json({ status: 'Erro', Erro: "Você deve digitar de 2 a 20 Números!" });
+    }else //Se o array tiver entre 2 a 20 números ele realiza a ordenação normalmente
+    {
+    vetor.sort((b, a) => b - a); //ordena o array
+    res.json({ status: 'ok', Números_Ordenados: vetor }); //mostra mensagem com o array ordenado no "response"
+    }
 })();
 
 //função para calcular e mostrar o menor e o maior número
 const menorMaior = (req, res) => (async () => {
+    //Pegando os valores mandado pelo POST do tests.rest
     const { vetor } = req.body;
 
-    // Salva na variavel "menor" o menor número do array
-    const menor = Math.min(...vetor);
+    if (vetor.length < 2 || vetor.length >20 ) {  //Verifica se o array possui menos que 2 valores ou mais que 20, nesse caso ele informa um erro
+        res.json({ status: 'Erro', Erro: "Você deve digitar de 2 a 20 Números!" }); //Mensagem informando o erro
+    }else //Se o array tiver entre 2 a 20 números ele realiza a ordenação normalmente
+    {
+    
+    const menor = Math.min(...vetor); // Salva na variavel "menor" o menor número do array
 
-    // Salva na variavel "maior" o maior número do array
-    const maior = Math.max(...vetor);
+    
+    const maior = Math.max(...vetor); // Salva na variavel "maior" o maior número do array
 
-    // Retorna o vetor ordenado e o menor e maior número
-    res.json({ status: 'ok', Menor: menor, Maior: maior });
+    res.json({ status: 'ok', Menor: menor, Maior: maior }); // Retorna o vetor ordenado e o menor e maior número
+    }
 })();
 
 module.exports = {
